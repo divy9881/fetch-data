b7cddd9d671d1babed42fbff737a2019634c8f41
Divy Patel <divy9881@gmail.com>
2023-09-29 00:11:04 -0500

    feat: add insights topic and update README
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# fetch-data
-Fetch Data Engineer: Home Assignment
+# fetch-backend
+Fetch-data: Home Assignment: Efficient Kafka Consumer implementation
+
+## Setup
+```
+# Pull the repository to the local machine
+$ git clone https://github.com/divy9881/fetch-data
+
+# Change directory to the fetch-data
+$ cd ./fetch-data
+
+# Install pip(pip install packages) dependencies
+$ pip install -r requirements.txt
+
+# Build and run containers/services defined in compose.yaml
+$ docker compose up
+
+# Start Kafka Consumer Data Processing
+$ python ./kafka-consumer.py
+```
+
+## File tree
+
+- **app.js** - Kafka Consumer code to process the user logins data and generate insights from it
+- **compose.yaml** - Definition of container services like Kafka Producer, Zookeeper as a control-plane, and Confluent-Kafka as a broker orchestrator
+- **requirements.txt** - Used by pip to keep track of external dependencies
+- **.gitignore** - To ignore python dependency directory and other beefy files, so as to not take up much git storage space
+
+## Code implementation and design considerations for performance
+- To introduce parallelism in consuming and processing user login records, I have created a pool of threads, where each thread can be pinned to a different CPU core to leverage the parallelism of isolation of cores.
+- Each thread processes a batch of user login data records. A thread generates insights from the batch of records and pushes this insights to the user-logins-insights topic.
+- The number of threads in a thread pool and the batch size can be fine tuned, by experimenting on various combinations, to increase the throughput and decrease the latency of the pipeline
+
+## How would you deploy this application in production?
+- Using docker compose, we can orchestrate various containers of services, and can be deployed to a cluster of nodes
+- A fully-managed Enterprise Kafka Cloud provider like Confluent can be used to deploy our production-ready data pipeline
+
+## What other components would you want to add to make this production ready?
+- Consumer groups can be used to allow consuming data by multiple consumers subscribed to same topic. New consumers can be added on the fly to the group as the load of incoming data increases.
+- Load balancer can be introduced to avoid hotspots in the consumer groups(i.e. a consumer node burdened with consuming a lot of data, while others are sitting idle)
+- A external service which could constantly auto-tune consumer configurations like batch-sizes, auto offset committing intervals, number of threads in a thread pool to manage the concurrency
+- Dynamic partition assignment can be done to automatically balance the partitions among consumer instances
+
+## How can this application scale with a growing dataset?
+- Multiple instances of consumers can be added to a consumer group, to increase the pace of consumption of data, and hence, to increase the throughput with increasing amount of incoming data
--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,13 +1,107 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+from threading import Thread
+import json
+import time
+import uuid
 
+# Configuration for the kafka consumer
+BOOTSTRAP_SERVERS =  'localhost:29092'
+INSIGHTS_TOPIC = 'user-login-insights'
+
+# Concurrency configurations
+# This can be fine tuned by experimenting with various values
+# to attain higher throughput and lower latencies
+MAX_RECORDS = 10
+THREAD_POOL_THREADS_COUNT = 10
+
+# Insights dictionaries to store in-memory insights on user logins data
+number_of_user_logins = 0
+device_type_insights = {}
+device_type_insights['android'] = 0
+device_type_insights['iOS'] = 0
+device_type_insights['NS'] = 0 # Not Specified
+locale_insights = {}
+
+# Initialize kafka consumer to user-login topic
 consumer = KafkaConsumer('user-login', # Topic name to subscribe to
                          group_id='user-login-analytics', # Group name
-                         bootstrap_servers=['localhost:29092'], # Connect to the bootstrap server
+                         bootstrap_servers=[BOOTSTRAP_SERVERS], # Connect to the bootstrap server
                          auto_offset_reset='earliest', # Start processing the logged messages from the start of the queue
                          enable_auto_commit=True # Enable auto offset commiting. By-default it is 5 seconds
                          )
 
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+# Poll batch of records from the Kafka consumer iterator and return the batch
+def poll_records(consumer, max_records):
+    num_records = 0
+    messages = []
+
+    for message in consumer:
+        messages.append(message)
+        num_records += 1
+        if num_records == max_records:
+            break
+
+    return messages
+
+# Initialize kafka producer to store user logins insights to user-login-insights topic
+producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVERS])
+
+# Add new topic to the Kafka cluster
+admin_client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
+if INSIGHTS_TOPIC not in admin_client.list_topics():
+    topic = NewTopic(name=INSIGHTS_TOPIC, num_partitions=1, replication_factor=1)
+    admin_client.create_topics(new_topics=[topic], validate_only=False)
+
+# Function which decodes the records and processes the user logins
+# to generate insights and store them to user-logins-insights topic
+def process_logins(logins):
+    for login in logins:
+        global number_of_user_logins
+        number_of_user_logins += 1
+        login = login.value.decode('utf-8')
+        login = json.loads(login)
+        locale = login['locale']
+        if 'device_type' in login:
+            device_type = login['device_type']
+        else:
+            device_type = 'NS'
+
+        if locale not in locale_insights:
+            locale_insights[locale] = 0
+        locale_insights[locale] += 1
+        device_type_insights[device_type] += 1
+        print(number_of_user_logins, locale_insights, device_type_insights)
+
+        insights = {}
+        insights['number_of_user_logins'] = number_of_user_logins
+        insights['locale_insights'] = locale_insights
+        insights['device_type_insights'] = device_type_insights
+        insights['timestamp'] = int(time.time())
+
+        producer.send(INSIGHTS_TOPIC, key=bytes(str(uuid.uuid4()), 'utf-8'), value=bytes(str(insights), 'utf-8'))
+        producer.flush()
+
+try:
+    while True:
+        num_threads_spawned = 0
+        threads = [0] * THREAD_POOL_THREADS_COUNT
+        # Spawn threads to process batch of user-login records from kafka consumer
+        # This ensures parallelization of data processing and hence, increases the performance
+        # and throughput of the data pipeline
+        while num_threads_spawned < THREAD_POOL_THREADS_COUNT:
+            logins = poll_records(consumer, 10)
+            threads[num_threads_spawned] = Thread(target=process_logins, args=[logins])
+            threads[num_threads_spawned].start()
+            num_threads_spawned += 1
+
+        # Wait for all the threads to complete before executing another batches of records
+        thread_wait_index = 0
+        while thread_wait_index < THREAD_POOL_THREADS_COUNT:
+            threads[thread_wait_index].join()
+            thread_wait_index += 1
+except KeyboardInterrupt:
+    pass
+
+producer.flush(30)
+producer.close()
